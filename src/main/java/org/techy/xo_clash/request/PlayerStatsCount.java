72d8025e100594e1d8b013bae3499c893a5f1e10
HenryTech12 <fakorodehenry@gmail.com>
2026-05-14 03:54:14 +0100

    fixed player stat counts repo issue

--- src/main/java/org/techy/xo_clash/request/PlayerStatsCount.java
+++ src/main/java/org/techy/xo_clash/request/PlayerStatsCount.java
@@ -1,8 +1,8 @@
 package org.techy.xo_clash.request;
 
 public record PlayerStatsCount(
-        long wins,
-        long losses,
-        long draws,
-        long totalGames
+        Long wins,
+        Long losses,
+        Long draws,
+        Long totalGames
 ) {}
