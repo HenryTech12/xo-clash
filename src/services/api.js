a37ff52ad639864bfe2935621bfc50ec6c27be19
Fakorode Odunayo Henry <156727288+HenryTech12@users.noreply.github.com>
2026-07-04 00:15:32 +0100

    Update API URL to new render service

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL = `${
-    import.meta.env.VITE_API_URL || "https://xo-clash-8ysf.onrender.com"
+    import.meta.env.VITE_API_URL || "https://xo-clash-cdtg.onrender.com"
 }/api/v1`;
 
 const api = axios.create({
